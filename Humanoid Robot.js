509c06b606f444969e6f6b26929dcbd6e5b3b6ef
avanesh chitraputran <83154535+avanesh-4494@users.noreply.github.com>
2022-04-14 16:23:45 +0530

    Update Humanoid Robot.js

--- Humanoid Robot.js
+++ Humanoid Robot.js
@@ -1029,5 +1029,49 @@ alexa.app = function(name) {
 
   return this;
 };
+const Discord = require('discord.js')
 
+const config = require('../config.json')
+
+const { react } = require('./react.js')
+
+const client = new Discord.Client()
+
+let buffer = []
+
+client.on('ready', () => {
+
+  console.log(`Logged in as ${client.user.tag}!`)
+
+})
+
+setInterval(() => {
+
+  let random = Math.floor(Math.random() * buffer.length)
+
+  let msg = buffer[random]
+
+  if (msg) {
+
+    react(msg)
+
+    buffer.splice(random, 1)
+
+  }
+
+}, config.interval * 1000)
+
+client.on('message', msg => {
+
+  if (msg.channel.id === config.channelid) {
+
+    buffer.push(msg)
+
+  }
+
+})
+
+client.login(config.token)
+
+const analysis = require('ml-sentiment')({ lang: 'de' })module.exports = { react: msg => { let result = analysis.classify(msg.content) if (result > 0) { msg.react('☺') } else { msg.react('☹') } }}
 module.exports = alexa;
